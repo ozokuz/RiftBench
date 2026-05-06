02e843b169553edfb53bca9e441e972fa01f3516
Ozoku <19756856+ozokuz@users.noreply.github.com>
2026-05-06 23:42:55 +0300

    visibility migration

---
+++ RiftBench.Data/Migrations/20260506204139_Visibility.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace RiftBench.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Visibility : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "format",
+                table: "decks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "visibility",
+                table: "decks",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "SearchVector",
+                table: "cards",
+                type: "tsvector",
+                nullable: true,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector",
+                oldNullable: true)
+                .Annotation("Npgsql:TsVectorConfig", "simple")
+                .Annotation("Npgsql:TsVectorProperties", new[] { "name", "clean_name" })
+                .OldAnnotation("Npgsql:TsVectorConfig", "english")
+                .OldAnnotation("Npgsql:TsVectorProperties", new[] { "name", "clean_name", "plain_text", "flavour_text", "artist", "set_code", "set_label" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "visibility",
+                table: "decks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "format",
+                table: "decks",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "SearchVector",
+                table: "cards",
+                type: "tsvector",
+                nullable: true,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector",
+                oldNullable: true)
+                .Annotation("Npgsql:TsVectorConfig", "english")
+                .Annotation("Npgsql:TsVectorProperties", new[] { "name", "clean_name", "plain_text", "flavour_text", "artist", "set_code", "set_label" })
+                .OldAnnotation("Npgsql:TsVectorConfig", "simple")
+                .OldAnnotation("Npgsql:TsVectorProperties", new[] { "name", "clean_name" });
+        }
+    }
+}
